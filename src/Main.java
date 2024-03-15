a4d3fcc83319d96f9e1629901d079e68656ed2c0
preetikaAccolite <162336578+preetikaAccolite@users.noreply.github.com>
2024-03-15 12:53:31 +0530

    Update Main.java
    "using treeset"

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.*;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 public class Main{
     public static void main(String[] args){
-        List<Integer> list1 = new ArrayList<Integer>();
-        list1.add(6);
-        list1.add(9);
+        Set<Integer> list1=new TreeSet<>();
+        list1.add(78);
         list1.add(1);
-        list1.add(8);
-        System.out.println("before sorting:"+list1);
-        Collections.sort(list1);
+        list1.add(10);
+        list1.add(2);
         System.out.println("After sorting:"+list1);
 
     }
